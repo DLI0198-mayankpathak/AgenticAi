c63dcd63b36f40b1fe9ac7d736e4be97934bb9ca
Mayank Pathak <mayank.pathak@godigit.com>
2025-11-17 10:55:44 +0530

    Clean up: Remove redundant documentation, old test files, and duplicate startup scripts

--- allow_firewall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Adding firewall rule for port 8000...
-netsh advfirewall firewall add rule name="Python API Server" dir=in action=allow protocol=TCP localport=8000
-echo Done! Port 8000 is now allowed through Windows Firewall.
-pause
